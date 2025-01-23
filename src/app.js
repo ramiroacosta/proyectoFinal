66a1fb368a5a2585d3d9d7af5133e5d816dbb620
Ramiro Acosta <109625447+ramiroacosta@users.noreply.github.com>
2025-01-23 11:25:17 -0300

    inicio de preentrega

--- src/app.js
+++ src/app.js
@@ -1,55 +1,17 @@
 import express from 'express';
+import productsRouter from './routes/products.Router.js'
+import cartsRouter from './routes/carts.Router.js'
+import __dirname from './utils.js';
+
 const app = express();
 const  PORT = 8080;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"))
+app.use('/api/product', productsRouter);
+app.use('/api/cart', cartsRouter);
 
-
-//API REST 
-
-app.get('/', (req, res) => {
-    res.send("test")
-})
-//endponts de users
-const users = []
-//listar usuarios
-app.get('/api/users', (req, res) => {
-    res.send(users)
-})
-//crear users
-app.post('/api/users', (req, res) => {
-    let user = req.body
-    //generar ID unico
-    const numRamdon = Math.floor(Math.random()*100 + 1);
-    user.id = numRamdon + users.length;
-    users.push(user); //agregamos user con el id nuevo
-    res.send(user); 
-})
-// actualizar 
-app.put('/api/users:userId', (req, res) => {
-    let userId = parseInt(req.params.userId)
-    let userUpdate = req.body
-    // buscamos por ID
-    let userPosition = users.findIndex(u => u.id === userId)
-    if(userPosition < 0) {
-        return res.status(404).send('User not found')
-    }
-    //actulizamos el user 
-    users[userPosition] = userUpdate
-    res.send(userUpdate)
-})
-
-//borrar
-app.delete('/api/users:userId', (req, res) => {
-    let userId = parseInt(req.params.userId)
-    const userPosition = users.findIndex((u=>u.id === userId))
-    if(userPosition < 0) {
-        return res.status(404).send('User not found')
-    }
-    //eliminamos el user
-    users.splice(userPosition, 1)
-    res.send("usuario eliminado");
-})
 
 //ESCUCHA
 app.listen(PORT, ()=> {
---
+++ src/routes/carts.Router.js
@@ -0,0 +1,17 @@
+import {Router} from 'express';
+
+const router = new Router();
+
+router.get('/',(req, res) => {
+    
+})
+router.post('/',(req, res) => {
+    
+})
+router.delete('/:cartId',(req, res) => {
+    
+})
+router.put('/:cartId',(req, res) => {
+    
+})
+export default router;
